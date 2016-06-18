95e3b2b0237c35a6922c9e8cb67624ea30792e1e
Peter Petrov <Peter.Petrov@brightpearl.com>
2016-06-18 21:44:44 +0300

    initial commit

---
+++ client.js
@@ -0,0 +1,171 @@
+"use strict";
+var net = require('net');
+var client = new net.Socket();
+// var readline = require('readline');
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+
+var myName = process.argv[2];
+var roomName = process.argv[3];
+
+
+var guests = [];
+var history = {};
+var isHost = false;
+
+//connect to server
+client.connect({port: 8124, host: '127.0.0.1'}, function() {
+  console.log("connected to server");
+  // Say we are new client. State name and room.
+  console.log("sending: " + "new|" + myName + "|" + roomName);
+  client.write("new|" + myName + "|" + roomName);
+
+  client.on('data', function(data) {
+    data = data.toString();
+    console.log("data coming from server: " + data);
+    var type = data.split('|')[0];
+    switch (type) {
+      case "host":
+        // I am host.
+        console.log('I am host');
+        isHost = true;
+        break;
+      case "guest":
+          console.log('I am guest, waiting for the host.');
+          // I am guest. Waiting for the host to contact me.
+          const clientServer = net.createServer((c) => {
+            c.on('data', function(buf) {
+              buf = buf.toString();
+              var inputType = buf.split('|')[0];
+              var inputData = buf.split('|')[1];
+              console.log("host ccontacted and is sending " + inputType);
+              console.log(inputData);
+              switch (input) {
+                case "history":
+                  history = JSON.parse(inputData);
+                  break;
+                case "guests":
+                  guests = connectToGuests(JSON.parse(inputData));
+                  break;
+                case "guest":
+                  guests.push = connectToGuests([JSON.parse(inputData)])[0];
+                  break;
+                default:
+              }
+            });
+          });
+          clientServer.listen(8125, () => {
+            console.log('guestServer bound');
+          });
+
+        break;
+      case "newGuest":
+        // new guest came and we are the host. connect and flush users and history.
+        let guestName = data.split('|')[1];
+        let guestHost = data.split('|')[2];
+        console.log('new guest came we need to flush all to him');
+        console.log('host: '+ guestHost);
+        console.log('name: '+ guestName);
+
+        let newClientSocket = new net.Socket();
+        newClientSocket.connect({port: 8125, host: guestHost}, function() {
+          newClientSocket.write("history|"+JSON.stringify(history));
+          newClientSocket.write("guests|"+JSON.stringify(guests.map((x) => { return {
+            guestHost : x.guestHost,
+            name : x.guestName
+          };})));
+        });
+        guests.push = {
+          guestHost : guestHost,
+          name : guestName,
+          clientSocket : newClientSocket
+        }
+
+        break;
+      default:
+    }
+  });
+
+  // client.on('close', () => {console.log('closiiiiiing');})
+  // client.on('end', () => {console.log('closiiiiiing');})
+
+	// rl.on('line', (input) => {
+	// 	client.write(myName + ": " + input);
+	// });
+});
+
+
+var connectToGuests = function(gs) {
+  return gs.map(function(x) {
+    let xClient = new net.Socket();
+    xClient.connect({port: 8125, host: x.guestHost}, function() {
+    });
+    x.clientSocket = xClient;
+    return x;
+  });
+}
+
+
+
+
+
+// var recordAndBroadast = function (message, sender) {
+// 	clients.map(function(x) {
+// 		// don't broadcast to the sender
+// 		if (x != sender) {
+// 			x.write(message);
+// 		}
+// 	});
+// 	history.push(message);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var net = require('net');
+//
+// var client = new net.Socket();
+//
+// var readline = require('readline');
+// var clientName = process.argv[2];
+//
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+//
+// client.connect({port: 8124, host: '127.0.0.1'}, function() {
+// 	rl.on('line', (input) => {
+// 		client.write(clientName + ": " + input);
+// 	});
+// });
+//
+// client.on('data', function(data) {
+// 	console.log(data + "");
+// });
+//
+// client.on('close', function() {
+// 	console.log('Connection closed');
+// });
---
+++ server.js
@@ -0,0 +1,101 @@
+"use strict";
+const net = require('net');
+var rooms = {};
+const server = net.createServer((c) => {
+  c.on('data', function(buf) {
+    buf = buf.toString();
+    console.log(buf);
+    var type = buf.split('|')[0];
+    console.log(type);
+    //type|name|clientRoom
+    switch (type) {
+      case "new":
+          let clientName = buf.split('|')[1];
+          let clientRoom = buf.split('|')[2];
+          if (!rooms[clientRoom]) {
+            rooms[clientRoom] = {
+              "host" : clientName,
+              "hostSocket" : c
+            };
+            c.write("host");
+            console.log('--------------------------');
+            console.log("host came: " + clientName);
+            console.log("host room: " + clientRoom);
+          } else {
+            c.write("guest");
+            console.log('--------------------------');
+            console.log("guest came: " + clientName);
+            console.log("guest room: " + clientRoom);
+            console.log("room host: " + clientRoom);
+            console.log("sending to host: " + "newGuest|"+clientName+"|"+c.remoteAddress.split(':')[3]);
+
+            rooms[clientRoom].hostSocket.write("newGuest|"+clientName+"|"+c.remoteAddress.split(':')[3]);
+            //TODO see how to close
+            c.end();
+          }
+        break;
+      default:
+
+    }
+    console.log("rooms: ");
+    console.log(rooms);
+	});
+});
+
+server.on('error', (err) => {
+  throw err;
+});
+server.listen(8124, () => {
+  console.log('server bound');
+});
+
+
+
+
+
+
+
+
+
+// const net = require('net');
+// var history = [];
+// var clients = [];
+// const server = net.createServer((c) => {
+// 	// Identify this client
+//   c.name = c.remoteAddress.split(':')[3] + ":" + c.remotePort 	//remember the new client
+// 	var i = clients.length;
+// 	clients.push(c);
+// 	//flush the history to the new client
+// 	history.map(x => c.write(x + '\n'));
+// 	//log and add to history
+// 	recordAndBroadast(('client ' + c.name + ' connected!'), {}); //we want to broadcast to everyone including sender
+//
+//   c.on('end', () => {
+// 		clients.splice(i, 1);
+// 		recordAndBroadast(('client ' + c.name + ' disconnected!'), c);
+//   });
+//
+// 	c.on('data', function(buf) {
+// 		recordAndBroadast(buf, c);
+// 	 });
+// });
+// server.on('error', (err) => {
+//   throw err;
+// });
+// server.listen(8124, () => {
+//   console.log('server bound');
+// });
+//
+// var recordAndBroadast = function (message, sender) {
+// 	clients.map(function(x) {
+// 		// don't broadcast to the sender
+// 		if (x != sender) {
+// 			x.write(message);
+// 		}
+// 	});
+// 	history.push(message);
+// }
+//
+// var broadcast = function (message) {
+// 	clients.map(x => x.write(message));
+// }
