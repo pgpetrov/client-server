d4ca3610f94d407e8f30ae9e2f88659009228af6
Peter Petrov <Peter.Petrov@brightpearl.com>
2016-06-19 23:16:11 +0300

    small refactoring

--- client.js
+++ client.js
@@ -29,67 +29,60 @@ client.connect({port: 8124, host: serverIp}, function() {
     var type = data.split('|')[0];
     switch (type) {
       case "host":
-        // I am host.
+        // Server says I am host.
         console.log("system> " + myName + ' is host');
         history.push("system> " + myName + ' is host');
         isHost = true;
         myIp = data.split('|')[1];
         break;
       case "guest":
-          // I am guest. Waiting for the host to contact me.
+          // Server says I am guest. Waiting for the host to contact me.
           const clientServer = net.createServer((c) => {
             c.on('data', function(buf) {
               buf = buf.toString();
               var inputType = buf.split('|')[0];
-              if (!initialized && inputType == "historyGuests") {
-                let historyData = buf.split('|')[1];
-                let guestsData = buf.split('|')[2];
-                history = JSON.parse(historyData);
-                guests = connectToGuests(JSON.parse(guestsData));
-                guests.map((x) => {
-                  if (x.isHost) {
-                    x.clientSocket = c;
+
+              switch (inputType) {
+                case "historyGuests":
+                  if (!initialized) {
+                    let historyData = buf.split('|')[1];
+                    let guestsData = buf.split('|')[2];
+                    history = JSON.parse(historyData);
+                    guests = connectToGuests(JSON.parse(guestsData));
+                    guests.map((x) => {
+                      if (x.isHost) {
+                        x.clientSocket = c;
+                      }
+                      return x;
+                    });
+                    initialized = true;
+                    history.map((x) => {console.log(x); return x;});
                   }
-                  return x;
-                });
-                initialized = true;
-                history.map((x) => {console.log(x); return x;});
-              } else if(inputType == "BECOMINGHOST") {
-                let commandComingFrom = c.remoteAddress.split(':')[3];
-                if (commandComingFrom == serverIp) {
-                  isHost = true;
-                  myIp = buf.split('|')[1];
-                  // I am host.
-                  console.log("system> " + myName + ' is host');
-                  history.push("system> " + myName + ' is host');
-                }
-              } else if (isHost && inputType=="newGuest") {
-                //we are the new host and we must take care of the new guests
-                handleGuestLogic(buf);
-              }else {
-                console.log(buf);
-                history.push(buf);
+                  break;
+                case "BECOMINGHOST":
+                  let commandComingFrom = c.remoteAddress.split(':')[3];
+                  if (commandComingFrom == serverIp) {
+                    //Server connected and promoted me to host.
+                    isHost = true;
+                    myIp = buf.split('|')[1];
+                    console.log("system> " + myName + ' is host');
+                    history.push("system> " + myName + ' is host');
+                  }
+                  break;
+                case "newGuest":
+                  //we are the new host and we must take care of the new guests
+                  handleGuestLogic(buf);
+                  break;
+                default:
+                  console.log(buf);
+                  history.push(buf);
               }
-
-              process.on('SIGINT', function() {
-                console.log('sigint inside');
-                c.end();
-                client.end();
-                process.exit();
-              });
-
-
             });
           });
-
           clientServer.on('error', (err) => {
             throw err;
           });
-
-          clientServer.listen(8125, () => {
-            // console.log('guestServer bound');
-          });
-
+          clientServer.listen(8125);
         break;
       case "newGuest":
         handleGuestLogic(data);
@@ -103,18 +96,24 @@ client.connect({port: 8124, host: serverIp}, function() {
 
 var connectToGuests = function(gs) {
   return gs.map(function(x) {
-    //do not connect to host he is allready connected to us.
+    // do not connect to host he is allready connected to us.
     if (!x.isHost) {
       let xClient = new net.Socket();
+      // connect to client x
       xClient.connect({port: 8125, host: x.guestIp}, function() {
+        // on data coming from client x write in history and flush
         xClient.on('data', function(buf) {
           buf = buf.toString();
           console.log(buf);
           history.push(buf);
         });
 
-        xClient.on('close', () => {
-            guests = guests.filter((y) => x.guestIp != y.guestIp);
+        // on end coming from client x remove object from guests.
+        xClient.on('end', () => {
+            console.log('---- disconnected coming from ---- ' + x.name);
+            guests = guests.filter((y) => {
+              return x.guestIp != y.guestIp;
+            });
         })
 
 
@@ -163,7 +162,7 @@ var handleGuestLogic = function (data) {
       isHost : true
     });
 
-    newClientSocket.on('close', () => {
+    newClientSocket.on('end', () => {
         console.log("clientSOCKET closed");
         broadcastAndSave("system> " + guestName + " disconnected!");
         guests = guests.filter((x) => x.guestIp != guestIp);
@@ -186,6 +185,7 @@ var handleGuestLogic = function (data) {
 process.on('SIGINT', function() {
   console.log('sigint outside');
   client.end();
+  guests.map(x => x.clientSocket.end());
   process.exit();
 });
 
